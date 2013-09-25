655a2df09ba2b9c0590b3f78ca3cd51be44c4a62
Josh Starcher <josh.starcher@gmail.com>
2013-09-25 12:07:49 -0400

    Use the uscm connection for sp_project_ministry_focuses

--- app/models/sp_project_ministry_focus.rb
+++ app/models/sp_project_ministry_focus.rb
@@ -1,4 +1,5 @@
 class SpProjectMinistryFocus < ActiveRecord::Base
+  establish_connection :uscm
 
   belongs_to :ministry_focus, :class_name => "SpMinistryFocus", :foreign_key => "ministry_focus_id"
   belongs_to :project, :class_name => "SpProject", :foreign_key => "project_id"
